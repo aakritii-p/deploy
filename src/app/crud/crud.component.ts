7d496f2b6cec08b747213fab3961b18d618e6ae6
Aakriti <aakriti.poudel@accolitedigital.com>
2022-04-15 23:14:16 +0530

    addSalesperson done

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -6,10 +6,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crud.component.css']
 })
 export class CrudComponent implements OnInit {
-
+  newSalespersonClicked=false;
+  salespersons=[
+    {
+       Id:1,Name:"Yash",Phone:9991,Password:9991,RmId:1
+    },
+    {
+      Id:2,Name:"Paru",Phone:9891,Password:9891,RmId:2
+     }
+  ];
+   color:any;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+   model:any={};
+   model2:any={};
+  //  salesperso:any={};
+  //  i:any={};
+
+   addSalesperson(){
+     this.salespersons.push(this.model);
+     this.model={}
+   }
+   deleteSalesperson(id:any){
+     this.salespersons.splice(id);
+     console.log(id);
+   }
+   myValue:any;
+
+  editSalesperson(editSalespersonInfo:any){
+       this.model2.Id=this.salespersons[editSalespersonInfo].Id;
+       this.model2.Name=this.salespersons[editSalespersonInfo].Name;
+       this.model2.Phone=this.salespersons[editSalespersonInfo].Phone;
+       this.model2.Password=this.salespersons[editSalespersonInfo].Password;
+       this.model2.RmId=this.salespersons[editSalespersonInfo].RmId;
+       this.myValue=editSalespersonInfo;
+  }
+
+  updateSalesperson(){
+    let editSalespersonInfo=this.myValue;
+    for(let i=0;i<this.salespersons.length;i++)
+    {
+      if(i==editSalespersonInfo){
+            this.salespersons[i]=this.model2;
+            this.model2={}
+      }
+    }
+  }
+  
+  addNewSalespersonBtn(){
+    this.newSalespersonClicked=!this.newSalespersonClicked;
+
+  }
+  
+  changeColorOne(){
+    this.color=!this.color;
+    if(this.color){
+      return '#ffffff';
+
+    }
+    else{
+      return '#f6f6f6';
+    }
+  }
+
+
 
 }
